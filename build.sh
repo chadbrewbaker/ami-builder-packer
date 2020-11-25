33daa436a9d95cfaad46c968a15147fa9b006083
Chad Brewbaker <crb002@gmail.com>
2020-11-25 02:33:12 -0600

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/bin/zsh
+yum update -y
+yum upgrade -y
+yum list gcc*
+yum list clang*
+# update yum packages
+# build ruby from source with a few extra gems from a gemfile
+# pull nginx,mysql
+#
